2439789717093f46c83a67b005c666a0c50bc078
Mohankumar Rathinam <mohan.cse17@gmail.com>
2021-09-30 20:49:17 +0400

    spring boot project created

---
+++ src/main/java/com/online/bookstore/OnlineBookstoreApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.online.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookstoreApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookstoreApiSpringBootApplication.class, args);
+	}
+
+}
